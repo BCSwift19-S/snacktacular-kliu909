f1775a4f5192cd866712e312963ba311c1c0faca
Katrina (Kejun) Liu <liuapg@bc.edu>
2019-03-31 16:54:16 -0400

    week 11 finished

--- Snacktacular/AppDelegate.swift
+++ Snacktacular/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Firebase
+import GooglePlaces
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        
+        GMSPlacesClient.provideAPIKey("AIzaSyAhtd3edLZhDRlTqXHe4Efr2IffGkb3jpo")
+    
+        FirebaseApp.configure()
         return true
     }
 
